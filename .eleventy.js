8902ee3da52fb6516e2e995998907504311475fd
mayormaier <maierandrewd@gmail.com>
2022-02-27 18:37:32 -0500

    fix link issue and add plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
 const embeds = require("eleventy-plugin-embed-everything");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(embeds);
+    eleventyConfig.addPlugin(syntaxHighlight);
+    
     eleventyConfig.addWatchTarget('_site/assets/*.css');
     eleventyConfig.setBrowserSyncConfig({
     files: ['_site/assets/*.css']
     });
     
-    return { dir: { input: "src" } };
+    return { 
+        dir: { input: "src" },
+        pathPrefix: "/11ty-hello-horse/",
+        jsDataFileSuffix: ".11tydata" 
+    };
 }
